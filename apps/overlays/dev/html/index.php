72b061a3918f1efa89b6d294dd95199589ca460b
Adrián Estévez Barreiro <aebarreiro@gradiant.org>
2022-07-05 09:51:05 +0200

    added env values to web

---
+++ apps/overlays/dev/html/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="main.css">  
+	</head>
+	<body>
+		<div>
+			<?php
+			echo "<p>Hello World from getenv('MY_NAMESPACE')</p>";
+			echo "<p>I'm on getenv('MY_NODE')</p>";
+			?>
+		</div>
+	</body>
+</html>
