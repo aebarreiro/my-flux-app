3c61acc43fbce111790d769f3b2f6dd5a7baed95
Adrian Estevez <aebarreiro@gradiant.org>
2022-07-05 11:25:02 +0200

    css

--- apps/overlays/dev/html/index.php
+++ apps/overlays/dev/html/index.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<link rel="stylesheet" href="main.css">  
+<?php echo '<link rel="stylesheet" type="text/css" href="main.css">'."\n"; ?>
 	</head>
 	<body>
 		<div>
-		<?php
-			$my_ns = getenv('MY_NAMESPACE') ;
-			$my_node = getenv('MY_NODE') ;
-			print "<p>Hello World from " . $my_ns . " </p>\n";
-			print "<p>I'm on " . $my_node . " </p>\n";
-		?>
+<?php
+$my_ns = getenv('MY_NAMESPACE') ;
+$my_node = getenv('MY_NODE') ;
+print "<p>Hello World from " . $my_ns . " </p>\n";
+print "<p>I'm on " . $my_node . " </p>\n";
+?>
 		</div>
 	</body>
 </html>
